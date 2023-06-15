9260c49d9c49bf6f3629bbda5b43543b7eb11119
RitaNicolette <rittyrdward34@gmail.com>
2023-06-15 08:44:40 -0700

    cisfun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like buikding a multilingual puzzle");
+return (0);
+}
